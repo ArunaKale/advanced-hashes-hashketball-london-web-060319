d83537bd27a8794a08d7b79f8ee5aef335d16bc1
AN K <a2nkale@gmail.com>
2019-06-03 13:33:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -41,3 +41,46 @@ def game_hash
         number: 1,
         shoe: 19,
         points: 26,
+  rebounds: 12,
+        assists: 6,
+        steals: 3,
+        blocks: 8,
+        slam_dunks: 5
+      },
+      "Jason Terry" => {
+        number: 31,
+        shoe: 15,
+        points: 19,
+        rebounds: 2,
+        assists: 2,
+        steals: 4,
+        blocks: 11,
+        slam_dunks: 1
+      }
+    }
+  },
+  away: {
+    team_name: "Charlotte Hornets",
+<<<<<<< HEAD
+    colors: ["Turquoise", "Purple"],
+=======
+    colors: ["Torquoise", "Purple"],
+>>>>>>> 113f32438f1b794b65aec172fc2815625ffccc27
+    :players => {
+      "Jeff Adrien" => {
+        number: 4,
+        shoe: 18,
+        points: 10,
+        rebounds: 1,
+        assists: 1,
+        steals: 2,
+        blocks: 7,
+        slam_dunks: 2
+      },
+        "Bismack Biyombo" => {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
